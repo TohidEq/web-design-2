b655116f2e422c8358cc1cd9433961b3d6c8acca
Tohid Eghdami <TohidEq@gmail.com>
2024-08-23 21:29:18 +0330

    add src

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  syntax: "postcss-scss",
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+function NavBar({}: Props) {
+  return (
+    <div className="navbar">
+      navbar brooo
+      <div className="d"></div>
+    </div>
+  );
+}
+
+export default NavBar;
